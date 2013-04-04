9424679960a9ba0db9ed5f868c34f45e97fc25a0
glathoud <glathoud@yahoo.fr>
2013-04-04 21:27:16 +0200

    fixed comment typo

--- explore/scheme_matmul_classic.scm
+++ explore/scheme_matmul_classic.scm
@@ -3,7 +3,7 @@
 ;;; Scheme - successfully tested with Gambit v4.6.2
 ;;; -*- mode:scheme; coding: utf-8 -*-
 ;;;
-;;; Iterative implementation directly translated from the JavaScript in ../examples.js
+;;; Imperative implementation directly translated from the JavaScript in ../examples.js
 
 (load "scheme_matmul_common.scm")
 
