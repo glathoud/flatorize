ba8fd6a37f86957eb8e01fa509150a4ad0b3b32f
glathoud <glathoud@yahoo.fr>
2013-04-09 06:53:55 +0200

    stub

---
+++ explore/c_matmul_classic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "c_matmul_common.c"
+
+extern const int NITER;
+extern const float mat_a[];
+extern const float mat_b[];
+extern const float mat_c[];
+
+int main()
+{
+  
+  printf("done\n");
+  return 0;
+}
