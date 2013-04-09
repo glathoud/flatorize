5ac24d561bb2433dac3bc3f082fb97a9b30b5dc9
glathoud <glathoud@yahoo.fr>
2013-04-09 06:52:24 +0200

    stub

---
+++ explore/c_matmul.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+reset
+
+gcc -o c_matmul_list_of_lists.bin c_matmul_list_of_lists.c
+
---
+++ explore/c_matmul_common.c
@@ -0,0 +1,15 @@
+static const int NITER = 5e5;
+
+static const float mat_a[3*4] = { 1, 2, 3, 4,
+                                  5, 6, 7, 8,
+                                  9, 10, 11, 12 };
+
+static const float mat_b[4*2] = { 13, 14,
+                                  15, 16,
+                                  17, 18,
+                                  19, 20 };
+
+static const float mat_c[3*2] = { 170, 180, 
+                                  426, 452,
+                                  682, 724 };
+
