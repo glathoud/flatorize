fb3ed6e02b2a92f1b0b8253d8951c7439c569dc5
glathoud <glathoud@yahoo.fr>
2013-04-10 06:58:44 +0200

    eliminated one assignment

--- explore/scheme_matmul.sh
+++ explore/scheme_matmul.sh
@@ -6,6 +6,8 @@ set -v
 
 time scheme_matmul_classic.scm          # Ugly imperative implementation, ported from ../examples.js
 
+time scheme_matmul_classic2.scm         # Ugly imperative implementation, ported from ../examples.js
+
 time scheme_matmul_list_of_lists.scm    # Concise functional implementation from rosettacode.org
 
 time scheme_matmul342.scm               # Proposed "flatorized" implementation for I:3,J:4,K:2
@@ -14,6 +16,10 @@ time scheme_matmul342.scm               # Proposed "flatorized" implementation f
 
 ( cat scheme_matmul_common.scm ; grep -v load scheme_matmul_classic.scm ) | grep -v gsi-script > tmp.scm
 gsc -exe -o scheme_matmul_classic.bin tmp.scm
+rm tmp.scm
+
+( cat scheme_matmul_common.scm ; grep -v load scheme_matmul_classic2.scm ) | grep -v gsi-script > tmp.scm
+gsc -exe -o scheme_matmul_classic2.bin tmp.scm
 rm tmp.scm
 
 ( cat scheme_matmul_common.scm ; grep -v load scheme_matmul_list_of_lists.scm ) | grep -v gsi-script > tmp.scm
@@ -27,6 +33,8 @@ rm tmp.scm
 # Done compiling. Now time the binaries:
 
 time scheme_matmul_classic.bin          # Ugly imperative implementation, ported from ../examples.js
+
+time scheme_matmul_classic2.bin         # Ugly imperative implementation, ported from ../examples.js
 
 time scheme_matmul_list_of_lists.bin    # Concise functional implementation from rosettacode.org
 
---
+++ explore/scheme_matmul_classic2.scm
@@ -0,0 +1,71 @@
+#!/usr/bin/env gsi-script
+
+;;; Scheme - successfully tested with Gambit v4.6.2
+;;; -*- mode:scheme; coding: utf-8 -*-
+;;;
+;;; Imperative implementation directly translated from the JavaScript in ../examples.js
+
+(load "scheme_matmul_common.scm")
+
+(define (sanity_check)
+  (equal? (matmul_classic mat_a mat_b 3 4 2)
+          mat_c)
+  )
+
+(define (speed_test)
+  (define (speed_test_impl) 
+    (let loop ((n NITER))
+      (if (< n 1)
+          #t
+          (let ((mat_c (matmul_classic mat_a mat_b 3 4 2)))
+            ;; (display mat_c)   ; for debugging
+            (loop (- n 1))
+            )
+          )
+      )
+    )
+  (if (sanity_check)
+      (speed_test_impl)
+      (display "ERROR: Sanity check failed!\n")
+      )
+  )
+
+(define (matmul_classic a b I J K)
+  (let ((c (make-vector (* I K))))
+    (let loop-row ((i 0))
+      (if (< i I)
+          (begin
+            (let ((a_offset (* i J))
+                  (c_offset (* i K))
+                  )
+              (let loop-col ((k 0))
+                (if (< k K)
+                    (begin
+                        (let loop-j ((j 0) (sum 0))   ;; difference with ..._classic.scm: eliminated `sum` assignment based on Marc Feeley's feedback
+                          (if (< j J)
+                              (loop-j (+ j 1)
+                                      (+ sum
+                                         (*
+                                          (vector-ref a (+ a_offset j))
+                                          (vector-ref b (+ (* j K)  k))
+                                          )
+                                         ))
+                              (vector-set! c (+ c_offset k) sum)
+                              )
+                          )
+                        (loop-col (+ k 1))
+                        )
+                    '()
+                    )
+                )
+              )
+            (loop-row (+ i 1))
+            )
+          '()
+          )
+      )
+    c
+    )
+  )
+
+(speed_test)
