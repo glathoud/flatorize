385fdf9c76a8b888047df36388bd4cefb3ad56be
glathoud <glathoud@yahoo.fr>
2013-04-26 07:35:24 +0200

    added sanity check + speed test for c_dftreal16flat

--- explore/C/c_dftreal16.sh
+++ explore/C/c_dftreal16.sh
@@ -5,15 +5,15 @@ set -v
 # Compiling
 #
 gcc -o c_dftreal16_print_NITER.bin    c_dftreal16_print_NITER.c 
-gcc -o c_dftreal16_cooley_tukey.bin   c_dftreal16_cooley_tukey.c
-gcc -o c_dftreal16_flatorized.bin     c_dftreal16_flatorized.c
+# xxx gcc -o c_dftreal16_cooley_tukey.bin   c_dftreal16_cooley_tukey.c
+gcc -o c_dftreal16flat.bin           c_dftreal16flat.c
 #
 # Testing
 #
 c_dftreal16_print_NITER.bin
 #
-time c_dftreal16_cooley_tukey.bin
+# xxx time c_dftreal16_cooley_tukey.bin
 #
-time c_dftreal16_flatorized.bin
+time c_dftreal16flat.bin
 #
 set +v
