f439751718738504f55085d316fd93e6443ac624
glathoud <glathoud@yahoo.fr>
2013-06-07 06:17:55 +0200

    blah

--- explore/C/cO3_dftreal16.sh
+++ explore/C/cO3_dftreal16.sh
@@ -6,15 +6,15 @@ set -v
 #
 gcc -O3 -c -o cO3_dftreal16_common.o    cO3_dftreal16_common.c
 gcc -o cO3_dftreal16_print_NITER.bin    cO3_dftreal16_common.o cO3_dftreal16_print_NITER.c 
-gcc -o cO3_dftreal16flat.bin           cO3_dftreal16_common.o cO3_dftreal16flat.c
 gcc -o cO3_dftreal16fftw3.bin          cO3_dftreal16_common.o cO3_dftreal16fftw3.c -lfftw3 -lm
+gcc -o cO3_dftreal16flat.bin           cO3_dftreal16_common.o cO3_dftreal16flat.c
 #
 # Testing
 #
 cO3_dftreal16_print_NITER.bin
 #
-time cO3_dftreal16flat.bin
-#
 time cO3_dftreal16fftw3.bin
+#
+time cO3_dftreal16flat.bin
 #
 set +v
--- explore/C/c_dftreal16.sh
+++ explore/C/c_dftreal16.sh
@@ -5,15 +5,15 @@ set -v
 # Compiling
 #
 gcc -o c_dftreal16_print_NITER.bin    c_dftreal16_print_NITER.c 
-gcc -o c_dftreal16flat.bin           c_dftreal16flat.c
 gcc -o c_dftreal16fftw3.bin          c_dftreal16fftw3.c -lfftw3 -lm
+gcc -o c_dftreal16flat.bin           c_dftreal16flat.c
 #
 # Testing
 #
 c_dftreal16_print_NITER.bin
 #
-time c_dftreal16flat.bin     # Not compiled with -O3
-#
 time c_dftreal16fftw3.bin    # FFTW3 lib was compiled with -O3 (speed advantage)
+#
+time c_dftreal16flat.bin     # Not compiled with -O3
 #
 set +v
