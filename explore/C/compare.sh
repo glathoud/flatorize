c9590bffff26dc182835713a68b25e6c1b1fda38
glathoud <glathoud@yahoo.fr>
2013-07-29 08:54:56 +0200

    to compare

---
+++ explore/C/compare.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+set -v
+./cO3_dftreal1024flat_msr_hh.bin
+./cO3_dftreal1024flat_msr_hh.bin.save
+./cO3_dftreal1024flat_msr_hh.bin
+./cO3_dftreal1024flat_msr_hh.bin.save
+./cO3_dftreal1024flat_msr_hh.bin
+./cO3_dftreal1024flat_msr_hh.bin.save
+set +v
